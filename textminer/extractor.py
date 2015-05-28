3f6590c306a566dcdac8fee9da28a9b6e40de73b
Manish Patel <maddypatel@gmail.com>
2015-05-28 10:01:07 -0400

    Phone numbers works for extractor

--- textminer/extractor.py
+++ textminer/extractor.py
@@ -3,10 +3,13 @@ import re
 def phone_numbers(text):
     phone_list = []
     ext_phone = r"(?:\(?(\d{3})\)?[\-\.]?\s*)?(\d{3})[\-\.]?(\d{4})"
-    match = re.search(ext_phone, text)
-    for i in match.groups():
+    match = re.findall(ext_phone, text)
+
+    print(match)
+
+    for num in match:
         a_list = []
-        a_list.append("(" + match.groups()[0] + ") ")
-        a_list.append(match.groups()[1] + "-" + match.groups()[2])
-    phone_list.append("".join(a_list))
+        a_list.append("(" + num[0] + ") " + num[1] + "-" + num[2])
+        phone_list.append("".join(a_list))
+        #phone_list.append(a_list)
     return phone_list
